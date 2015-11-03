a29ccdcb3614c31de2274f886bede545381ce588
codeology <coder451@codeology.co.nz>
2015-11-04 11:33:32 +1300

    basic define element

--- src/EtchNode.js
+++ src/EtchNode.js
@@ -1,6 +1,48 @@
 
     function EtchNode( tag, asyncCallback ) {
 
+
+        function handleObject( changes ){
+
+            changes.forEach( function( change ) {
+
+                if ( change.object["parent"]  && change.name === "parent" && !( change.object[ "parent" ] instanceof EtchNode ) ){
+                    change.object["parent"] = null;
+                    return;
+                }
+
+                this.asyncCallback( null,  change );
+
+            }, this );
+        }
+
+        function handleChildNodes( changes ) {
+
+            changes.forEach(function (change) {
+
+                if ( change.type === "splice") {
+
+                    index = change.index;
+
+                    if ( change.removed.length  && !(change.object[index] instanceof EtchNode)){
+                        return;
+                    }
+
+                } else {
+                    index = change.object.length - 1;
+                }
+
+                if ( !( change.object[index] instanceof EtchNode ) ){
+                    change.object.splice( index, 1 );
+                    this.asyncCallback( new Error( "cannot append invalid node" ), null  );
+                    return;
+                }
+
+                this.asyncCallback( null, change );
+
+            }, this);
+        }
+
         Object.defineProperties( this, {
             asyncCallback:{
                 value:asyncCallback
@@ -17,88 +59,28 @@
             }
         });
 
-        this.observeObjectOrArray(this);
-        Array.observe( this.childNodes, this.handleChildNodes.bind( this ) );
+        Object.observe( this, handleObject.bind( this ) );
+        Array.observe( this.childNodes, handleChildNodes.bind( this ) );
     }
 
-
-
-    EtchNode.prototype.observeObjectOrArray = function( obj ){
-        if ( Array.isArray( obj ) ){
-            Array.observe( obj, this.handleArray.bind( this ) );
-        } else {
-            if ( obj !== null && typeof obj === "object") {
-                Object.observe( obj, this.handleObject.bind( this ) );
-            }
-        }
-    };
-
-    EtchNode.prototype.unobserveObjectOrArray = function( obj ){
-        if ( Array.isArray( obj ) ){
-            Array.unobserve( obj );
-        } else {
-            if ( obj !== null && typeof obj === "object") {
-                Object.unobserve( obj  );
-            }
-        }
+    EtchNode.prototype.__elementIndex__ = function( tag ){
+        return typeof this.__elementIndex__[tag]
     };
 
     EtchNode.prototype.root = function(){
         return this.parent ? this.parent.root() : this;
     };
 
-
-    EtchNode.prototype.handleArray = function( changes ) {
-        changes.forEach(function (change) {
-            this.asyncCallback( null, change );
-        }, this);
-    };
-
-    EtchNode.prototype.handleObject = function( changes ){
-
-        changes.forEach( function( change ) {
-
-            if ( change.object["parent"]  && change.name === "parent" && !( change.object[ "parent" ] instanceof EtchNode ) ){
-                change.object["parent"] = null;
-                return;
-            }
-
-            if ( change.type === "add" ){
-                this.observeObjectOrArray( change.object[ change.name ] );
-            }
-
-            this.asyncCallback( null,  change );
-
-        }, this );
+    EtchNode.prototype.defineElement = function( tag ){
+        this.__elementIndex__[tag] = {
+            "tagName": tag
+        };
     };
 
 
-
-    EtchNode.prototype.handleChildNodes = function( changes ) {
-
-        changes.forEach(function (change) {
-
-            if ( change.type === "splice") {
-
-                index = change.index;
-
-                if ( change.removed.length  && !(change.object[index] instanceof EtchNode)){
-                    return;
-                }
-
-            } else {
-                index = change.object.length - 1;
-            }
-
-            if ( !( change.object[index] instanceof EtchNode ) ){
-                change.object.splice( index, 1 );
-                this.asyncCallback( new Error( "cannot append invalid node" ), null  );
-                return;
-            }
-
-            this.asyncCallback( null, change );
-
-        }, this);
+    EtchNode.prototype.hasElementType = function( tag ){
+        return typeof this.__elementIndex__[tag]
     };
+
 
     module.exports = EtchNode;
--- test/EtchNode-test.js
+++ test/EtchNode-test.js
@@ -5,36 +5,32 @@
     var EtchNode = require("../src/EtchNode");
 
 
-    describe('Etch Node Functional', function(){
+    describe('EtchNode Basic Functionality Tests', function(){
 
-        it('should create a standard etch node', function(done){
+        it('should instantiable and have 2 default properties, parent and childNodes', function(done){
 
             var node = new EtchNode();
 
-            assert.equal( node.parent, null );
-            assert.deepEqual( node.childNodes,[] );
-
-            node.childNodes.push({});
-            node.parent={};
-
+            assert.equal( node.parent, null, "parent should be null because node is not yet included in a dom tree" );
+            assert.deepEqual( node.childNodes,[],"childNodes is an array" );
 
             done();
         });
 
-        it('should add a parent', function(done){
+        it('should add a parent EtchNode', function(done){
 
             var node = new EtchNode( null, function(){
-                assert( node.parent instanceof EtchNode );
+                assert( this.parent instanceof EtchNode );
                 done();
             });
 
             node.parent=new EtchNode();
         });
 
-        it('should add a child', function(done){
+        it('should add a child EtchNode', function(done){
 
             var node = new EtchNode( null, function(){
-                assert.equal( node.childNodes.length, 1 );
+                assert.equal( this.childNodes.length, 1 );
                 done();
             });
 
@@ -46,7 +42,7 @@
         it('should reject non EtchNode objects as a parent', function(done){
 
             var node = new EtchNode( null, function(){
-                assert.equal( node.parent, null );
+                assert.equal( this.parent, null );
                 done();
             });
 
@@ -54,33 +50,42 @@
         });
 
 
-
         it('should reject non EtchNode objects as childnodes', function(done){
 
             var node = new EtchNode( null, function(){
-                assert.equal( node.childNodes.length,0 );
+                assert.equal( this.childNodes.length,0 );
                 done();
             });
             node.childNodes.push({});
+
         });
 
-        it('should monitor plain arrays', function(done){
 
-            var node = new EtchNode( null, function( err, change ){
 
-                if ( change.type === "splice"){
-                    assert.equal( node.arr[0], 1 );
-                    done();
-                }
+        it('should append child nodes', function(done){
+
+            var node = new EtchNode( null, function(){
+                assert.equal( this.childNodes.length,0 );
+                done();
             });
+            node.childNodes.push({});
 
-            node.prop={};
-            node.arr = [];
-
-            setTimeout( function(){
-                node.arr.push(1);
-            },0);
+        });
 
 
+    });
+
+
+
+    describe('EtchNode.defineElement', function() {
+
+        it('should create a new element index containing a reference to the custom element by name', function (done) {
+
+            var node = new EtchNode();
+            node.defineElement("custom");
+
+            assert( node.hasElementType("custom") );
+
+            done();
         });
     });
