592159fa3006420bc269745fa72aa412a1367a16
codeology <coder451@codeology.co.nz>
2015-11-12 17:25:24 +1300

    etch element now uses etch node as its base inheritance and allows overriding etchnodes methods

---
+++ src/EtchElement.js
@@ -0,0 +1,181 @@
+
+
+    "use strict";
+
+    var EtchNode = require("etch-node");
+
+    function EtchElement( tag, nodeType ){
+
+        Object.defineProperty( this, "tagName",{
+            enumerable:true,
+            value: tag
+        });
+
+        this.parent = null;
+        this.childNodes = [];
+        this.nodeType = nodeType;
+    }
+    EtchElement.prototype.validNodes = new Set([1, 3, 9]);
+
+    EtchElement.prototype.createDocument = function(){
+        var Document = EtchNode.extend( EtchElement );
+        return new Document("DOCUMENT", 9 );
+    };
+
+    EtchElement.prototype.createElement = function( tag ){
+        var Element = EtchNode.extend( EtchElement );
+        return new Element( tag.toUpperCase(), 1 );
+    };
+
+    EtchElement.prototype.addEventListener = function( type, callback, capture ){
+        this.___addEventListener___( Infinity, type, callback, capture );
+    };
+
+    EtchElement.prototype.appendChild = function( node ) {
+
+        if ( !this.validNodes.has(node.nodeType) ){
+            return;
+        }
+
+        this.childNodes.push( node );
+
+        var event = this.createEventObject();
+        event.currentTarget = node;
+        this.triggerEvent( "onAppendChild", event );
+    };
+
+
+    module.exports = EtchNode.extend( EtchElement );
+
+
+//    function EtchDomNode( tag, xxx ) {
+//
+//        var init = function(){};
+//        var update = function(){};
+//        var destroy = function(){};
+//
+//        if ( typeof xxx === "function"){
+//            init = xxx;
+//            update = xxx;
+//            destroy = xxx;
+//        }
+//
+//        if ( typeof xxx === "object"){
+//
+//            if ( typeof xxx.init === "function"){
+//                init = xxx.init;
+//            }
+//
+//            if ( typeof xxx.update === "function") {
+//                update = xxx.update;
+//            }
+//
+//            if ( typeof xxx.destroy === "function"){
+//                destroy = xxx.destroy;
+//            }
+//        }
+//
+//        function handleObject( update, changes ){
+//
+//            changes.forEach( function( change ) {
+//
+//                if ( change.object["parent"]  && change.name === "parent" && !( change.object[ "parent" ] instanceof EtchDomNode ) ){
+//                    change.object["parent"] = null;
+//                    return;
+//                }
+//console.log( "--------->",  change.type, change.name, change.object[change.name], update)
+//                update.call( this, change.type, change.name, change.object[change.name] );
+//
+//            }, this );
+//        }
+//
+//        function handleChildNodes( tag, xxx, changes ) {
+//
+//            changes.forEach(function (change) {
+//
+//                if ( change.type === "splice") {
+//
+//                    index = change.index;
+//
+//                    if ( change.removed.length  && !(change.object[index] instanceof EtchDomNode)){
+//                        return;
+//                    }
+//
+//                } else {
+//                    index = change.object.length - 1;
+//                }
+//
+//                if ( !( change.object[index] instanceof EtchDomNode ) ){
+//                    change.object.splice( index, 1 );
+//                    xxx.call( this, new Error( "cannot append invalid node" ), null  );
+//                    return;
+//                }
+//
+//                xxx.call( this, null, change );
+//
+//            }, this);
+//        }
+//
+//        if ( typeof tag === "string" ){
+//            Object.defineProperty( this, "tagName",{
+//                value:tag.toUpperCase()
+//            });
+//        }
+//        Object.defineProperties( this, {
+//            parent:{
+//                value:null,
+//                writable: true,
+//                enumerable: true
+//            },
+//            childNodes:{
+//               value:[],
+//                writeable: true,
+//                enumerable: true
+//            }
+//        });
+//
+//        Object.observe( this, handleObject.bind( this, update ) );
+//        Array.observe( this.childNodes, handleChildNodes.bind( this,tag, xxx ) );
+//
+//        init.call( this, "init" );
+//    }
+//
+//    EtchDomNode.prototype.__EtchElementIndex__ = function( tag ){
+//        return typeof this.__EtchElementIndex__[tag]
+//    };
+//
+//    EtchDomNode.prototype.root = function(){
+//        return this.parent ? this.parent.root() : this;
+//    };
+//
+//    EtchDomNode.prototype.createEtchElement = function( tag ){
+//
+//        var lifecycle = function(){};
+//
+//        if ( typeof tag === "string"){
+//            tag = tag.toUpperCase();
+//        }
+//
+//        if ( this.hasEtchElementType( tag ) ){
+//            lifecycle = this.__EtchElementIndex__[tag].lifecycle;
+//        }
+//
+//        return new EtchDomNode( tag, lifecycle );
+//    };
+//
+//    EtchDomNode.prototype.defineEtchElement = function( tag, lifecycle ){
+//        tag = tag.toUpperCase();
+//        this.__EtchElementIndex__[tag] = {
+//            "tagName": tag,
+//            "lifecycle": lifecycle || {}
+//        };
+//    };
+//
+//
+//    EtchDomNode.prototype.hasEtchElementType = function( tag ){
+//        tag = tag.toUpperCase();
+//        return typeof this.__EtchElementIndex__[tag];
+//    };
+//
+//
+//    module.exports = EtchDomNode;
---
+++ test/EtchDom-test.js
@@ -0,0 +1,230 @@
+
+    "use strict";
+
+    var assert = require('assert');
+
+    var EtchElement = require("../src/EtchElement");
+
+
+
+    describe('EtchElement', function() {
+
+        it('should create a document', function (done) {
+
+            var element = new EtchElement();
+
+            var document = element.createDocument() ;
+
+            assert.equal( document.tagName, "DOCUMENT" );
+            assert.equal( document.parent, null, "parent should be null because node is not yet included in a dom tree");
+            assert.deepEqual( document.childNodes, [], "childNodes is an array");
+
+            done();
+        });
+
+
+    });
+
+
+    describe('Document', function() {
+
+        it('should create an element', function (done) {
+
+            var document = new EtchElement().createDocument();
+            var element = document.createElement("div");
+
+            assert.equal( element.tagName, "DIV" );
+            assert.equal( element.parent, null );
+            assert.deepEqual( element.childNodes, [] );
+
+            done();
+        });
+
+        it('should disallow appending of non EtchNode objects', function (done) {
+
+            var document = new EtchElement().createDocument();
+
+            document.appendChild({});
+
+            assert.equal( document.childNodes.length, 0 );
+
+            done();
+        });
+
+        it('should allow appending of EtchNode objects', function (done) {
+
+            var document = new EtchElement().createDocument();
+
+            document.appendChild( document.createElement("div") );
+
+            assert.equal( document.childNodes.length, 1 );
+
+            done();
+        });
+
+        it('should be notified when a child is appended via public event bus', function (done) {
+
+            var document = new EtchElement().createDocument();
+
+            var childElement = document.createElement("div");
+
+            document.addEventListener( "onAppendChild", function( e ){
+                assert(e.currentTarget === childElement);
+                done();
+            });
+
+            document.appendChild( childElement );
+        });
+    });
+
+//
+//    describe('EtchDomNode Basic Functionality Tests', function(){
+//
+//        it('should instantiable and have 2 default properties, parent and childNodes', function(done){
+//
+//            var node = new EtchDomNode();
+//
+//            assert.equal( node.parent, null, "parent should be null because node is not yet included in a dom tree" );
+//            assert.deepEqual( node.childNodes,[],"childNodes is an array" );
+//
+//            done();
+//        });
+//
+//        it('should add a parent EtchDomNode', function(done){
+//
+//            var node = new EtchDomNode( null, function( type ){
+//                if ( type !== "init"){
+//                    assert( this.parent instanceof EtchDomNode );
+//                    done();
+//                }
+//
+//            });
+//
+//            node.parent=new EtchDomNode();
+//        });
+//
+//        it('should add a child EtchDomNode', function(done){
+//
+//            var node = new EtchDomNode( null, function( type ){
+//                if ( type !== "init"){
+//                    assert.equal( this.childNodes.length, 1 );
+//                    done();
+//                }
+//            });
+//
+//            node.childNodes.push(new EtchDomNode());
+//        });
+//
+//
+//
+//        it('should reject non EtchDomNode objects as a parent', function(done){
+//
+//            var node = new EtchDomNode( null, function(type){
+//                if ( type !== "init") {
+//                    assert.equal(this.parent, null);
+//                    done();
+//                }
+//            });
+//
+//            node.parent={};
+//        });
+//
+//
+//        it('should reject non EtchDomNode objects as childnodes', function(done){
+//
+//            var node = new EtchDomNode( null, function(type){
+//                if ( type !== "init") {
+//                    assert.equal(this.childNodes.length, 0);
+//                    done();
+//                }
+//            });
+//            node.childNodes.push({});
+//
+//        });
+//
+//
+//
+//        it('should append child nodes', function(done){
+//
+//            var node = new EtchDomNode( null, function(type){
+//                if ( type !== "init") {
+//                    assert.equal(this.childNodes.length, 0);
+//                    done();
+//                }
+//            });
+//            node.childNodes.push({});
+//
+//        });
+//
+//
+//    });
+//
+//
+//
+//    describe('EtchDomNode.defineElement', function() {
+//
+//        var node = new EtchDomNode();
+//
+//        it('should register a new element in the index containing a reference to the custom element by name', function (done) {
+//
+//            node.defineElement("custom", {
+//
+//                init:function(){
+//
+//                }
+//            });
+//
+//            assert( node.hasElementType("custom") );
+//
+//            done();
+//        });
+//
+//        it('should create a new element by tag name', function (done) {
+//
+//            var el =  node.createElement("custom");
+//
+//            assert( el.tagName === "CUSTOM" );
+//
+//            done();
+//        });
+//
+//
+//    });
+//
+//    describe('EtchDomNode Element Lifecycle', function() {
+//
+//        it('should trigger init event on element created', function (done) {
+//
+//            var node = new EtchDomNode();
+//
+//            node.defineElement("img", {
+//                init:function(){
+//                    done();
+//                }
+//            });
+//
+//            node.createElement("img" );
+//
+//        });
+//
+//        it('should trigger update event on element', function (done) {
+//
+//            var node = new EtchDomNode();
+//
+//            node.defineElement("img", {
+//                update:function( type, prop, val ){
+//console.log("=============",arguments)
+//                    assert( type === "ADD");
+//                    assert( prop === "src");
+//                    assert( val === "12345");
+//
+//                    done();
+//                }
+//            });
+//
+//            node.createElement("img" );
+//            node.src="12345";
+//
+//        });
+//
+//    });
